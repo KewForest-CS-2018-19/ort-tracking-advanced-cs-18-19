b1bc4aa7eb2848fa303200a324cc8861d8dec0db
caitig131 <31958666+caitig131@users.noreply.github.com>
2018-12-03 08:20:46 -0500

    Update application/models.py

--- application/models.py
+++ application/models.py
@@ -3,7 +3,11 @@ from application import db
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     notes = db.Column(db.String(128), index=True, unique=False)
-#add columns here 
+    name = db.Column(db.String(128), index=True, unique=False)
+    product name = db.Column(db.String(128), index=True, unique=False)
+    product description = db.Column(db.String(128), index=True, unique=False)
+    help = db.Column(db.String(128), index=True, unique=False)
+#add columns here
     def __init__(self, notes):
         self.notes = notes
 
