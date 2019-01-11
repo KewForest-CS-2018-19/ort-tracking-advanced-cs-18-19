ebfb40984a580f8b3222adbbe0d5249d96428da8
WeijiaMa123 <31965430+WeijiaMa123@users.noreply.github.com>
2019-01-11 14:07:20 -0500

    change

--- application/models.py
+++ application/models.py
@@ -18,3 +18,12 @@ class Data(db.Model):
 
     def __repr__(self):
         return '<Data %r>' % self.notes
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    date = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
