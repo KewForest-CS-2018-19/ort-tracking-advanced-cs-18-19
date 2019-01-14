18f10d266f8137fc047d42e35aa83fed029be073
caitig131 <31958666+caitig131@users.noreply.github.com>
2019-01-14 08:12:05 -0500

    w

--- application/models.py
+++ application/models.py
@@ -3,13 +3,13 @@ from application import db
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     notes = db.Column(db.String(128), index=True, unique=False)
-    tdate = db.Column(db.String(128), index=True, unique=False)
+    date = db.Column(db.String(128), index=True, unique=False)
     weight_of_ort = db.Column(db.String(128), index=True, unique=False)
     weight_of_compost = db.Column(db.String(128), index=True, unique=False)
     groups = db.Column(db.String(128), index=True, unique=False)
 
 #add columns here
-    def __init__(self, notes, uname, product_name, product_description, help):
+    def __init__(self, notes, date, weight_of_ort, weight_of_compost, groups):
         self.notes = notes
         self.date = date
         self.weight_of_ort = weight_of_ort
