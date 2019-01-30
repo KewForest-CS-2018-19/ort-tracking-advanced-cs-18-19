06a17e414cec5a8af4a5b25021c86f0d2d0338c4
Hunter <hmilles2020@kewforest.org>
2019-01-30 09:59:34 -0500

    h

--- application/models.py
+++ application/models.py
@@ -4,6 +4,29 @@ from application import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import date
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(128), index=True, unique=False)
+    password_hash= db.Column(db.String(128))
+    posts = db.relationship('Data', backref = 'author', lazy ='dynamic' )
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+
+def load_user(id):
+    return User.query.get(int(id))
+
+
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     notes = db.Column(db.String(128), index=True, unique=False)
@@ -11,6 +34,7 @@ class Data(db.Model):
     weight_of_ort = db.Column(db.String(128), index=True, unique=False)
     weight_of_compost = db.Column(db.String(128), index=True, unique=False)
     groups = db.Column(db.String(128), index=True, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 #add columns here
@@ -23,22 +47,3 @@ class Data(db.Model):
 
     def __repr__(self):
         return '<Data %r>' % self.notes
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(128), index=True, unique=False)
-    password = db.Column(db.String(128))
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
