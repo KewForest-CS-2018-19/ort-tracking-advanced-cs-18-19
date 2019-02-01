709152480559b0e9ff479125aaaff7ea49ab2ab2
WeijiaMa123 <31965430+WeijiaMa123@users.noreply.github.com>
2019-02-01 08:46:29 -0500

    login

--- application/routes.py
+++ application/routes.py
@@ -61,8 +61,8 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(url_for('index'))
+            next_page = url_for('bview')
+        return redirect(url_for('bview'))
     return render_template('login.html', title='Sign In', form=form)
 
 @application.route('/logout')
