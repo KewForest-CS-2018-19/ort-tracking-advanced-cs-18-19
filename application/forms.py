64fe5789573a07581e9ebb8dbd33635b1906c961
Andy Liu <andyliu@Andys-MacBook-Pro-2.local>
2019-04-02 11:02:00 -0400

    routes and forms

--- application/forms.py
+++ application/forms.py
@@ -28,6 +28,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     school = StringField('School', validators=[])
+    schoolid = NumberField('Schoolid', validators=[])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
