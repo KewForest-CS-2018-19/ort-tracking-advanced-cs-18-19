00ba6af72db77335ed57ca717b418a1d2604e05f
Andy Liu <andyliu@Andys-MacBook-Pro-2.local>
2019-04-15 12:34:19 -0400

    a

--- application/routes.py
+++ application/routes.py
@@ -27,7 +27,7 @@ def bview():
     try:
         #num_return = int(form2.numRetrieve.data)
         #query_db = Data.query.order_by(Data.id.desc())#took out .limit(num_return)
-        query_db = Data.query.filter(Data.school).order_by(Data.id.desc())#took out .limit(num_return)
+        query_db = Data.query.filter(Data.school_id).order_by(Data.id.desc())#took out .limit(num_return)
         for q in query_db:
             print(q.notes)
         db.session.close()
